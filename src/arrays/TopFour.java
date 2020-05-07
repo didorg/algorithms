043711bda6b5e727b1ec20e312fed4f7856530e1
Didier <didorg@gmail.com>
2020-05-07 16:59:50 -0400

    Array top four maximum value

---
+++ src/arrays/TopFour.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+
+/** Return an array with the top four maximum value **/
+public class TopFour {
+
+  public static void main(String[] args) {
+    int[] arr = {1232, -1221, 0, 345, 78, 99, 159};
+
+    System.out.println(Arrays.toString(topFour(arr)));
+  }
+
+  public static int[] topFour(int[] arr){
+    Arrays.sort(arr);
+
+    int[] topFour = Arrays.copyOfRange(arr, arr.length-4, arr.length);
+    /*** @return a new array containing the specified range from the original array */
+    int[] lowerFour = Arrays.copyOfRange(arr, 0,4);
+
+    return topFour;
+  }
+}
