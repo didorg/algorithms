dcb6827a484cda61a98055cf5b39c6ce892dc670
Didier <didorg@gmail.com>
2021-08-09 17:23:12 -0400

    refactor to @Test

--- src/arrays/LeftRotation.java
+++ src/arrays/LeftRotation.java
@@ -1,27 +1,30 @@
 package arrays;
 
-import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class LeftRotation {
-  public static void main(String[] args) {
-    //int n = 20;
-    int d = 10;
-    int[] a = {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51};
-    // Expected: 77 97 58 1 86 58 26 10 86 51 41 73 89 7 10 1 59 58 84 77
-    
-    System.out.println(
-        "The array after performing " + d + " left rotations is " + Arrays.toString(rotLeft(a, d)));
-  }
 
-  static int[] rotLeft(int[] a, int d) {
-    int result[] = new int[a.length];
-    for (int i = a.length - 1; i >= 0; i--) {
-      if (i - d < 0) {
-        result[a.length - (-1 * (i - d))] = a[i];
-      } else
-        result[i - d] = a[i];
+    static int[] rotLeft(int[] a, int d) {
+        int result[] = new int[a.length];
+        for (int i = a.length - 1; i >= 0; i--) {
+            if (i - d < 0) {
+                result[a.length - (-1 * (i - d))] = a[i];
+            } else
+                result[i - d] = a[i];
+        }
+
+        return result;
     }
 
-    return result;
-  }
+    @Test
+    public void rotLeftTest() {
+        int d = 10;
+        int[] a = {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51};
+        int[] expected = {77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77};
+        int[] result = rotLeft(a, d);
+
+        Assertions.assertArrayEquals(expected, result);
+
+    }
 }
