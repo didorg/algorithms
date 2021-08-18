40413b2e9fb3e597fd18389f7f7a5f83852f685c
Didier <didorg@gmail.com>
2021-08-18 19:17:10 -0400

    Greatest Common Divisor

---
+++ src/recursion/GCommDiv.java
@@ -0,0 +1,15 @@
+package recursion;
+
+// Greatest Common Divisor 
+public class GCommDiv {
+    public static void main(String[] args) {
+        System.out.println(gcd(8, 12));
+    }
+
+    // Applying Euclidean Algorithm
+    private static int gcd(int n, int m) {
+        if(n<0 || m<0) return -1;
+        if(m == 0) return n;
+        return gcd(m, n%m);
+    }
+}
