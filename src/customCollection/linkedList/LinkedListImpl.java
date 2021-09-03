25e066748d5874cdc3ea36451eae81c2940fa286
Didier <didorg@gmail.com>
2021-09-03 09:31:01 -0400

    add Generic LinkedList

--- src/customCollection/linkedList/LinkedListImpl.java
+++ src/customCollection/linkedList/LinkedListImpl.java
@@ -1,18 +1,18 @@
 package customCollection.linkedList;
 
 public class LinkedListImpl {
-  // Test code - main function
-  public static void main(String[] args) {
-    LinkedList ll = new LinkedList();
-    System.out.println(ll.getSize());
-    ll.add(8);
-    System.out.println(ll.getSize());
-    ll.add("String");
-    ll.add(5);
-    ll.add(10);
-    System.out.println(ll.find("String").getData());
-    ll.remove(5);
-    System.out.println("Size is " + ll.getSize());
-    System.out.println(ll.find(5));
-  }
+    // Test code - main function
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        System.out.println(ll.getSize());
+        ll.add(8);
+        System.out.println(ll.getSize());
+        ll.add("String");
+        ll.add(5);
+        ll.add(10);
+        System.out.println(ll.find("String").getData());
+        ll.remove(5);
+        System.out.println("Size is " + ll.getSize());
+        System.out.println(ll.find(5));
+    }
 }
--- src/customCollection/linkedList/Node.java
+++ src/customCollection/linkedList/Node.java
@@ -2,33 +2,34 @@ package customCollection.linkedList;
 
 // Node class
 public class Node<T> {
-  Node next;
-  T data;
+    Node next;
+    T data;
 
-  public Node() {}
+    public Node() {
+    }
 
-  public Node(T val) {
-    data = val;
-  }
+    public Node(T val) {
+        data = val;
+    }
 
-  public Node(T val, Node next) {
-    data = val;
-    this.next = next;
-  }
+    public Node(T val, Node next) {
+        data = val;
+        this.next = next;
+    }
 
-  public Node getNext() {
-    return next;
-  }
+    public Node getNext() {
+        return next;
+    }
 
-  public void setNext(Node next) {
-    this.next = next;
-  }
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
-  public T getData() {
-    return data;
-  }
+    public T getData() {
+        return data;
+    }
 
-  public void setData(T data) {
-    this.data = data;
-  }
+    public void setData(T data) {
+        this.data = data;
+    }
 }
